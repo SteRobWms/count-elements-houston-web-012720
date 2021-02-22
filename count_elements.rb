d7f5fde7cd53f6c0320a623569d55b0ddb2898ca
SteRobWms <stephen.r.williams.87@gmail.com>
2021-02-22 17:19:42 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+    counter_hash = {}
+    array.each do |element|
+        if counter_hash[element]
+            counter_hash[element] += 1
+        else
+            counter_hash[element] = 1
+        end
+    end
+    return counter_hash
 end
  
